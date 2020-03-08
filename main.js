8e854fabc3b1f89b478204e53ad4b6f6d1b55903
Matt Thornberry <mtberry16@hotmail.com>
2020-03-08 14:24:01 -0600

    Add push method

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-import { allocate, free, copy, get, set } from "./memory";
+import memory from "./memory";
 
 class Array {
   //initialize array
   constructor() {
     this.length = 0;
-    this.ptr = allocate(this.length);
+    this.ptr = memory.allocate(this.length);
+  }
+
+  //Add value to array by pushing
+  push(value) {
+    this._resize(this.length + 1);
+    memory.set(this.ptr + this.length, value);
+    this.length++;
   }
 }
